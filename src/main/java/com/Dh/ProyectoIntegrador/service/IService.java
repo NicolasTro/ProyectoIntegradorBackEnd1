7380c1a16d3bc9bfd15eb0671655e54012adb650
Nicolas <nicolas@naussa.com.uy>
2024-03-09 16:47:52 -0300

    Actualizacion del merge

--- src/main/java/com/Dh/ProyectoIntegrador/service/IService.java
+++ src/main/java/com/Dh/ProyectoIntegrador/service/IService.java
@@ -10,12 +10,12 @@ public interface IService<T> {
 	T guardar(T t);
 	T buscarPorId(Integer id);
 	void eliminar (Integer id);
-<<<<<<< HEAD
-	void actualizar(T t);
-=======
+
+
+
 	void actualizar(T t) throws OdontologoException;
 
->>>>>>> e8b143eda05d742e63b28e0d5f3638cbbe3703f4
+
 	List<T> listarTodos();
 
 

d9aeb483c6bcab0857998a51f9c456db6b2ed859
Bautista Sanguinetti <bautiscsc.123@gmail.com>
2024-03-20 00:04:05 -0300

    Funcionando el actualizar Paciente. No Actualiza bien la tabla de Domicilios!!!!

--- src/main/resources/static/js/update_paciente.js
+++ src/main/resources/static/js/update_paciente.js
@@ -1,35 +1,41 @@
-
 window.addEventListener('load', function(e){
-    
-    const formulario = document.querySelector("#formOdontologoUpdate");
-    let id;
-    
 
-    
+    const formulario = document.querySelector("#updatePaciente");
+    let id;
+
+
+
     document.addEventListener('click', function(e){
-        
+
         if(e.target instanceof(HTMLButtonElement)){
             if(e.target.textContent=="Modificar"){
 
                 id = e.target.dataset.id
-                // console.log("Id seleccionado: "+ id)
+                 console.log("Id seleccionado: "+ id)
             }
         }
     });
-    
+
 
     formulario.addEventListener('submit', function (e) {
 //        e.preventDefault()
   console.log(formulario);
   console.log("actualizacion")
   const formData = {
-      id,
-      nombre: document.querySelector("#nombreUpdate").value,
-    apellido: document.querySelector("#apellidoUpdate").value,
-    matricula: document.querySelector("#matriculaUpdate").value
+    id,
+    nombre: document.querySelector("#nombrePacienteUpdate").value,
+    apellido: document.querySelector("#apellidoPacienteUpdate").value,
+    dni: document.querySelector("#dniUpdate").value,
+    fechaIngreso: document.querySelector("#fechaIngresoUpdate").value,
+    domicilio : {
+        calle: document.querySelector("#domicilioCalleUpdate").value,
+        numero: document.querySelector("#domicilioNumeroUpdate").value,
+        localidad: document.querySelector("#domicilioLocalidadUpdate").value,
+        provincia: document.querySelector("#domicilioProvinciaUpdate").value,
+    },
     }
-    
-    const url = '/odontologos/actualizar';
+
+    const url = '/pacientes/actualizar';
        const settings = {
            method: 'PUT',
            headers: {
@@ -37,27 +43,24 @@ window.addEventListener('load', function(e){
             },
             body: JSON.stringify(formData)
        }
-       
+
        fetch(url, settings)
            .then(response => response.json())
            .then(data => {
                 let successAlert = '<div class="alert alert-success alert-dismissible">' +
                     '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
-                    '<strong></strong> Odontólogo Actualizado </div>'
+                    '<strong></strong> Paciente Actualizado </div>'
 
-                document.querySelector('#response').innerHTML = successAlert;
-                document.querySelector('#response').style.display = "block";
+                document.querySelector('#responsePatientUpdate').innerHTML = successAlert;
+                document.querySelector('#responsePatientUpdate').style.display = "block";
 
-                
+
            })
            .catch(error => {
              let errorAlert = "<div class='alert alert-danger alert-dismissible'>"+
-                                    "<button type='button' class='close' data-dismiss='alert'>&times;</button>"+ 
+                                    "<button type='button' class='close' data-dismiss='alert'>&times;</button>"+
                                     "<strong> Error intente nuevamente</strong></div>"
 
-//console.log(error)
-//                     document.querySelector('#response').innerHTML = errorAlert;
-//                     document.querySelector('#response').style.display = "block";
 
                     });
 
@@ -65,14 +68,13 @@ window.addEventListener('load', function(e){
        let pathname = window.location.pathname;
        if(pathname === "/"){
            document.querySelector(".nav .nav-item a:first").addClass("active");
-       } else if (pathname == "/odotologoLista.html") {
+       } else if (pathname == "/pacienteLista.html") {
            document.querySelector(".nav .nav-item a:last").addClass("active");
        }
     });
-    
+
 });
 
 
 }
 )
-

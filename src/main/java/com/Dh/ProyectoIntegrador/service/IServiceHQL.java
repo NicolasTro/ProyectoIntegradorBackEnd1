bdc31eac7e87160e4f99ee8b4ac8bc2192c2b84a
Nicolas <nicolas@naussa.com.uy>
2024-03-22 17:36:38 -0300

    Odontologo Front responsive hasta 768px

--- src/main/java/com/Dh/ProyectoIntegrador/service/IServiceHQL.java
+++ src/main/java/com/Dh/ProyectoIntegrador/service/IServiceHQL.java
@@ -1,9 +1,5 @@
 package com.Dh.ProyectoIntegrador.service;
 
-import com.Dh.ProyectoIntegrador.entity.Odontologo;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 import java.util.Optional;
 
 //@Service

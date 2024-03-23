dd971ec7bc681f374dfa180eedfbe8ec5b6966b3
Bautista Sanguinetti <bautiscsc.123@gmail.com>
2024-03-22 23:18:18 -0300

    Andando IServiceHQL en Pacientes

--- src/main/java/com/Dh/ProyectoIntegrador/repository/IPacienteRepository.java
+++ src/main/java/com/Dh/ProyectoIntegrador/repository/IPacienteRepository.java
@@ -1,11 +1,20 @@
 package com.Dh.ProyectoIntegrador.repository;
 
 import com.Dh.ProyectoIntegrador.entity.Domicilio;
+import com.Dh.ProyectoIntegrador.entity.Odontologo;
 import com.Dh.ProyectoIntegrador.entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface IPacienteRepository extends JpaRepository<Paciente, Long> {
-    
+
+    @Query("SELECT p FROM Paciente p WHERE p.nombre =?1")
+    Optional<List<Paciente>> findByNombre(String valor);
+    @Query("SELECT p FROM Paciente p WHERE p.apellido =?1")
+    Optional<List<Paciente>> findByApellido(String valor);
 }

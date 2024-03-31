aa121bdcc276cb008d5640a4a6e332800172440e
Nicolas <nicolas@naussa.com.uy>
2024-03-30 23:09:12 -0300

    Pruebas de integracion de odontologo

--- src/test/java/com/Dh/ProyectoIntegrador/ProyectoIntegradorApplicationTest.java
+++ src/test/java/com/Dh/ProyectoIntegrador/ProyectoIntegradorApplicationTest.java
@@ -3,8 +3,12 @@ package com.Dh.ProyectoIntegrador;
 import com.Dh.ProyectoIntegrador.dto.odontologos.OdontologoDTO;
 import com.Dh.ProyectoIntegrador.dto.odontologos.request.OdontologoRequestDTO;
 
+import com.Dh.ProyectoIntegrador.dto.odontologos.response.OdontologoResponseDTOFull;
+import com.Dh.ProyectoIntegrador.excepciones.ResourceNotDeletedException;
+import com.Dh.ProyectoIntegrador.excepciones.ResourceNotFoundException;
 
 import com.Dh.ProyectoIntegrador.service.implementacion.OdontologoService;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -12,37 +16,111 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-	@Slf4j
+@Slf4j
 @SpringBootTest
+@Transactional
 class ProyectoIntegradorApplicationTest {
 
 
-@Autowired
+	@Autowired
 
 	private OdontologoService odontologoService;
 
+	@Test
+	public void guardarOdontologoEnService() {
 
+		OdontologoRequestDTO odontologoAGuardar = new OdontologoRequestDTO();
 
+		odontologoAGuardar.setApellido("ApellidoPrueba");
+		odontologoAGuardar.setNombre("NombrePrueba");
+		odontologoAGuardar.setMatricula("MatriculaPrueba");
+
+		OdontologoResponseDTOFull odontologoGuardado = (OdontologoResponseDTOFull) odontologoService.guardar(odontologoAGuardar);
+		assertEquals(odontologoAGuardar.getNombre(), odontologoGuardado.getNombre());
+		assertEquals(odontologoAGuardar.getApellido(), odontologoGuardado.getApellido());
+		assertEquals(odontologoAGuardar.getMatricula(), odontologoGuardado.getMatricula());
+	}
 
 	@Test
-	public void guardarOdontologo() {
+	public void eliminarOdontologoEnService() {
+
+		Long indiceOdontologoAEliminar = -1L;
+
+		assertThrows(ResourceNotDeletedException.class, () -> {
+			odontologoService.eliminar(indiceOdontologoAEliminar);
+		});
 
 
-
-		OdontologoRequestDTO od = new OdontologoRequestDTO();
-
-
-		od.setApellido("apellidoJero");
-		od.setNombre("nombreJero");
-		od.setMatricula("matriculaPatente");
-
-
-		OdontologoDTO odNUevo = odontologoService.guardar(od);
-
-log.info(odNUevo.getNombre());
 	}
+
+	@Test
+	public void modificarOdontologoEnService() {
+
+		OdontologoRequestDTO odontologoAActualizar = new OdontologoRequestDTO();
+		odontologoAActualizar.setId(30L);
+		odontologoAActualizar.setNombre("NombrePruebaActualizar");
+		odontologoAActualizar.setApellido("ApellidoPruebaActualizar");
+		odontologoAActualizar.setMatricula("MatricualPruebaActualizar");
+
+		assertThrows(ResourceNotFoundException.class, ()->{
+			odontologoService.actualizar(odontologoAActualizar);
+		});
+	}
+
+@Test
+	public void listarOdontologosEnService(){
+
+	OdontologoRequestDTO odontologoAGuardar1 = new OdontologoRequestDTO();
+	OdontologoRequestDTO odontologoAGuardar2 = new OdontologoRequestDTO();
+
+	odontologoAGuardar1.setApellido("ApellidoPrueba1");
+	odontologoAGuardar1.setNombre("NombrePrueba1");
+	odontologoAGuardar1.setMatricula("MatriculaPrueba1");
+
+
+	odontologoAGuardar2.setApellido("ApellidoPrueba2");
+	odontologoAGuardar2.setNombre("NombrePrueba2");
+	odontologoAGuardar2.setMatricula("MatriculaPrueba2");
+
+	OdontologoResponseDTOFull odontologoGuardado1 = (OdontologoResponseDTOFull) odontologoService.guardar(odontologoAGuardar1);
+	OdontologoResponseDTOFull odontologoGuardado2 = (OdontologoResponseDTOFull) odontologoService.guardar(odontologoAGuardar2);
+	List<OdontologoDTO> listaOdontologosDTO = odontologoService.listarTodos();
+
+	assertThat(listaOdontologosDTO).hasSizeGreaterThan(1);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
